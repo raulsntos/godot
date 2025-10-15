a6d1f8b0c349821f2ecca4955a7dd4efb71dd126
Raul Santos <raulsntos@gmail.com>
2025-10-15 22:39:50 +0200

    [WIP] Make C#-related files show up in FileSystem dock

--- editor/settings/editor_settings.cpp
+++ editor/settings/editor_settings.cpp
@@ -694,7 +694,7 @@ void EditorSettings::_load_defaults(Ref<ConfigFile> p_extra_config) {
 	// FileSystem
 	EDITOR_SETTING(Variant::INT, PROPERTY_HINT_RANGE, "docks/filesystem/thumbnail_size", 64, "32,128,16")
 	_initial_set("docks/filesystem/always_show_folders", true);
-	_initial_set("docks/filesystem/textfile_extensions", "txt,md,cfg,ini,log,json,yml,yaml,toml,xml");
+	_initial_set("docks/filesystem/textfile_extensions", "txt,md,cfg,ini,log,json,yml,yaml,toml,xml,cs,csproj,slnx");
 	_initial_set("docks/filesystem/other_file_extensions", "ico,icns");
 	_initial_set("docks/filesystem/automatically_open_created_scripts", true);
 
